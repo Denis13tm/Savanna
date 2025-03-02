b4111633d1480f503dc449215a70da4467c82536
Otabek <mrdunacdtm@gmail.com>
2025-03-03 02:11:15 +0900

    Initial Commit

---
+++ Savanna/SavannaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SavannaApp.swift
+//  Savanna
+//
+//  Created by Otabek Tuychiev on 2025/03/03.
+//
+
+import SwiftUI
+
+@main
+struct SavannaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
